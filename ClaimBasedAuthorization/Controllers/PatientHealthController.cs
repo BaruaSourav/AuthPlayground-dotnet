208e2febc39db5872c2da3e9d150cea8138262af
Sourav Barua <sourav.shr6@gmail.com>
2022-01-05 23:08:25 -0500

    Demo controller with views

---
+++ ClaimBasedAuthorization/Controllers/PatientHealthController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClaimBasedAuthorization.Controllers
+{
+    public class PatientHealthController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult PatientHealthInfo()
+        {
+            return View();
+        }
+        
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+    }
+}
