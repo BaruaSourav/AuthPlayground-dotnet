10d39534ba88447dc22416b13d314fd8cc19f887
Sourav Barua <sourav.shr6@gmail.com>
2022-01-05 22:27:58 -0500

    basic app config

--- ClaimBasedAuthorization/Program.cs
+++ ClaimBasedAuthorization/Program.cs
@@ -1,8 +1,25 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Claim-based authorization project working");
+if(app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+else if (app.Environment.IsProduction())
+{
+    app.UseHttpsRedirection();
+}
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapGet("/heartbeat", () => $"Claim-based authorization project working : {DateTime.Now.ToLongDateString()}");
+app.MapDefaultControllerRoute();
+app.MapControllers();
 
 app.Run();
